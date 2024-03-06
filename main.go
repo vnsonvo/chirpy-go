1dbd4eb9e3a71a9522906e37b5f365c5b6dd684d
Son Vo <vnsonvo@gmail.com>
2024-03-07 00:16:23 +0700

    add validate chirp endpoint

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -75,9 +77,78 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func handlerValidate(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	const maxchirpLength = 140
+	if len(params.Body) > maxchirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	val := sanitizeFunc(params.Body)
+
+	respondWithJSON(w, http.StatusOK, returnVals{CleanedBody: val})
+}
+
+func sanitizeFunc(val string) string {
+	var profane = map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+
+	str := strings.Split(val, " ")
+	for i, v := range str {
+		key := strings.ToLower(v)
+		if _, ok := profane[key]; ok {
+			str[i] = "****"
+		}
+	}
+	return strings.Join(str, " ")
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, msg string) {
+	if statusCode > 499 {
+		log.Printf("Responding with 5xx error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, statusCode, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(statusCode)
+	w.Write(data)
+}
+
 func apiRouter(apiConf *apiConfig) http.Handler {
 	r := chi.NewRouter()
 	r.Get("/healthz", handlerReadiness)
 	r.Get("/reset", apiConf.handlerReset)
+	r.Post("/validate_chirp", handlerValidate)
 	return r
 }
